44dfaf7d86da1d2eca7def38cd160f1bc156fd30
bmvenkat25 <bmvenkat25@gmail.com>
2023-09-17 18:22:13 +0530

    LongestCommonPrefix Solution

--- src/main/java/LongestCommonPrefix/LongestCommonPrefixSolution.java
+++ src/main/java/LongestCommonPrefix/LongestCommonPrefixSolution.java
@@ -2,6 +2,21 @@ package LongestCommonPrefix;
 
 public class LongestCommonPrefixSolution {
     public static String longestCommonPrefix(String[] strs) {
-        return "ans";
+        String res = "";
+        for(int i=0;i<strs[0].length();i++) {
+            int flag = 0 ;
+            char x = strs[0].charAt(i);
+            for(int j=0;j<strs.length;j++) {
+                if(i < strs[j].length() && x == strs[j].charAt(i))
+                    flag++;
+                else
+                    break;
+            }
+            if(flag == strs.length)
+                res+=x;
+            else
+                break;
+        }
+        return res;
     }
 }
